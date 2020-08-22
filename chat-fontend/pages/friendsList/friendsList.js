2e0a2aeb9a96937ee69e5500b9c3782fd9637fc5
loversugar <253262275@qq.com>
2020-08-22 08:33:20 +0800

    添加消息

--- chat-fontend/pages/friendsList/friendsList.js
+++ chat-fontend/pages/friendsList/friendsList.js
@@ -5,7 +5,36 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    chatList: [
+      {
+        id: 1,
+        profile: "/images/image1.jpeg",
+        date: "星期一",
+        name: "张三",
+        content: "今天出去带球吧"
+      },
+      {
+        id: 2,
+        profile: "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=126511607,385840140&fm=26&gp=0.jpg",
+        date: "星期一",
+        name: "李四",
+        content: "哈哈哈"
+      },
+      {
+        id: 3,
+        profile: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598065791987&di=672b3e504b1268b81a3663033cbe153e&imgtype=0&src=http%3A%2F%2Fgb.cri.cn%2Fmmsource%2Fimages%2F2007%2F03%2F26%2Fem070325047.jpg",
+        date: "星期一",
+        name: "Totti",
+        content: "哈哈哈"
+      },
+      {
+        id: 4,
+        profile: "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1598065791987&di=eafd49858ec49e01a830ea53aa4fdb3c&imgtype=0&src=http%3A%2F%2Fg.hiphotos.baidu.com%2Fzhidao%2Fpic%2Fitem%2F5366d0160924ab183979fb8a37fae6cd7b890b1b.jpg",
+        date: "星期一",
+        name: "孙",
+        content: "哈哈哈"
+      }
+    ]
   },
 
   /**
